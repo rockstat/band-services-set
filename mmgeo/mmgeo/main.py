f7355e07865d84aaf7cf397e3cd81622c02c233f
Dmitry Rodin <madiedinro@gmail.com>
2018-06-22 17:23:22 +0300

    compact output, caching support

--- mmgeo/mmgeo/main.py
+++ mmgeo/mmgeo/main.py
@@ -3,6 +3,7 @@ import os.path
 import maxminddb
 from band import dome, logger, settings, RESULT_INTERNAL_ERROR, RESULT_NOT_LOADED_YET
 from prodict import Prodict
+from async_lru import alru_cache
 """
 Library docs: https://github.com/maxmind/MaxMind-DB-Reader-python
 
@@ -27,13 +28,132 @@ async def download_db():
         logger.exception('download err')
 
 
-@dome.expose(role=dome.ENRICHER, register=dict(key=['in.gen.track']))
-async def enrich(td, **params):
+def handle_location(city=None, country=None, subdivisions=None, **kwargs):
+    result = Prodict()
+    if country:
+        result.country_en = country['names']['en']
+        result.country_ru = country['names']['ru']
+        result.country_iso = country['iso_code']
+    if city:
+        result.city_en = city['names']['en']
+        result.city_ru = city['names']['ru']
+    if subdivisions and len(subdivisions) > 0:
+        region = subdivisions[0]
+        result.region_en = region['names']['en']
+        result.region_ru = region['names']['ru']
+        result.region_iso = region['iso_code']
+    return result
+
+
+@alru_cache(maxsize=256)
+async def locate(ip):
     try:
         if state.db:
-            location = state.db.get(td['ip'])
-            return location
+            location = state.db.get(ip)
+            return handle_location(**location)
         return {'error': RESULT_NOT_LOADED_YET}
     except Exception:
         logger.exception('mmgeo error')
     return {'error': RESULT_INTERNAL_ERROR}
+
+
+@dome.expose(
+    role=dome.ENRICHER,
+    register=dict(key=['in.gen.track'], props=dict(ip='td.ip')))
+async def enrich(td, **params):
+    return await locate(td['ip'])
+
+
+@dome.expose()
+async def cache_info():
+    return locate.cache_info()
+
+
+@dome.expose()
+async def get(ip, **params):
+    return await locate(ip)
+
+
+"""
+Full struct
+-----------
+{
+    "city": {
+        "geoname_id": 524901,
+        "names": {
+            "de": "Moskau",
+            "en": "Moscow",
+            "es": "Moscú",
+            "fr": "Moscou",
+            "ja": "モスクワ",
+            "pt-BR": "Moscovo",
+            "ru": "Москва",
+            "zh-CN": "莫斯科"
+        }
+    },
+    "continent": {
+        "code": "EU",
+        "geoname_id": 6255148,
+        "names": {
+            "de": "Europa",
+            "en": "Europe",
+            "es": "Europa",
+            "fr": "Europe",
+            "ja": "ヨーロッパ",
+            "pt-BR": "Europa",
+            "ru": "Европа",
+            "zh-CN": "欧洲"
+        }
+    },
+    "country": {
+        "geoname_id": 2017370,
+        "iso_code": "RU",
+        "names": {
+            "de": "Russland",
+            "en": "Russia",
+            "es": "Rusia",
+            "fr": "Russie",
+            "ja": "ロシア",
+            "pt-BR": "Rússia",
+            "ru": "Россия",
+            "zh-CN": "俄罗斯"
+        }
+    },
+    "location": {
+        "accuracy_radius": 1000,
+        "latitude": 55.7522,
+        "longitude": 37.6156,
+        "time_zone": "Europe/Moscow"
+    },
+    "postal": {
+        "code": "102617"
+    },
+    "registered_country": {
+        "geoname_id": 2017370,
+        "iso_code": "RU",
+        "names": {
+            "de": "Russland",
+            "en": "Russia",
+            "es": "Rusia",
+            "fr": "Russie",
+            "ja": "ロシア",
+            "pt-BR": "Rússia",
+            "ru": "Россия",
+            "zh-CN": "俄罗斯"
+        }
+    },
+    "subdivisions": [
+        {
+            "geoname_id": 524894,
+            "iso_code": "MOW",
+            "names": {
+                "de": "Moskau",
+                "en": "Moscow",
+                "es": "Moscú",
+                "fr": "Moscou",
+                "ru": "Москва"
+            }
+        }
+    ]
+}
+"""
--- sxgeo/sxgeo/main.py
+++ sxgeo/sxgeo/main.py
@@ -3,6 +3,7 @@ from pysyge.pysyge import GeoLocator, MODE_BATCH, MODE_MEMORY
 from prodict import Prodict
 import subprocess
 import os
+from async_lru import alru_cache
 
 state = Prodict()
 
@@ -27,13 +28,88 @@ async def startup():
         logger.exception('download err')
 
 
-@dome.expose(role=dome.ENRICHER, register=dict(key=['in.gen.track']))
-async def enrich(td = {}, **params):
+def handle_location(city=None, country=None, region=None, **kwargs):
+    result = Prodict()
+    if country:
+        result.country_en = country['name_en']
+        result.country_ru = country['name_ru']
+        result.country_iso = country['iso']
+    if city:
+        result.city_en = city['name_en']
+        result.city_ru = city['name_ru']
+    if region:
+        result.region_en = region['name_en']
+        result.region_ru = region['name_ru']
+        result.region_iso = region['iso']
+    return result
+
+
+@alru_cache(maxsize=256)
+async def locate(ip):
+    try:
+        if hasattr(state, 'geodata'):
+            location = Prodict.from_dict(
+                state.geodata.get_location(ip, detailed=True))
+            if not location or not location.info:
+                return {}
+            return handle_location(**location.info)
+        return {'result': RESULT_INTERNAL_ERROR}
+    except Exception:
+        logger.exception('mmgeo error')
+    return {'error': RESULT_INTERNAL_ERROR}
+
+
+@dome.expose(
+    role=dome.ENRICHER,
+    register=dict(key=['in.gen.track'], props=dict(ip='td.ip')))
+async def enrich(td={}, **params):
     """
     Handle incoming request
     sxg lib api details: https://github.com/idlesign/pysyge
     """
-    if hasattr(state, 'geodata'):
-        location = state.geodata.get_location(td['ip'], detailed=True)
-        return location
-    return {'result': RESULT_INTERNAL_ERROR}
+    return await locate(td['ip'])
+
+
+@dome.expose()
+async def cache_info():
+    return locate.cache_info()
+
+
+@dome.expose()
+async def get(ip, **kwargs):
+    return await locate(ip)
+
+
+"""
+Full struct
+-----------
+{
+    "city": "Дубна",
+    "country_id": 185,
+    "country_iso": "RU",
+    "fips": "0",
+    "info": {
+        "city": {
+            "id": 564719,
+            "lat": 56.73333,
+            "lon": 37.16667,
+            "name_en": "Dubna",
+            "name_ru": "Дубна"
+        },
+        "country": {
+            "id": 185,
+            "iso": "RU",
+            "lat": 60.0,
+            "lon": 100.0,
+            "name_en": "Russia",
+            "name_ru": "Россия"
+        },
+        "region": {
+            "id": 524925,
+            "iso": "RU-MOS",
+            "name_en": "Moskovskaya Oblast'",
+            "name_ru": "Московская область"
+        }
+    },
+}
+"""
