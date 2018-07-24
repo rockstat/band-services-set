f02a68538576731236aa85d69c8b9a9c3caa1e96
Dmitry Rodin <madiedinro@gmail.com>
2018-07-25 02:04:42 +0300

    pos class, fix buld opts

--- director/director/structs.py
+++ director/director/structs.py
@@ -7,4 +7,7 @@ IMAGE_CATEGORIES = Prodict(user='user', collection='collection', base='base')
 ImageObj = namedtuple('ImageObj', 'name category path')
 
 
-ServicePostion = namedtuple('ServicePostion', 'col row')
+class ServicePostion(namedtuple('ServicePostion', 'col row')):
+    __slots__ = ()
+    def __str__(self):
+        return f"{self.col}x{self.row}"
