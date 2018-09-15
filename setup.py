ded61e88ac7fd46b23829d9dc8df8ae9907096fe
Dmitry Rodin <madiedinro@gmail.com>
2018-09-15 14:39:32 +0300

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='band-services',
-    version='0.2.0',
+    version='0.2.1',
     author='Dmitry Rodin',
     author_email='madiedinro@gmail.com',
     license='MIT',
