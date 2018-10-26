d6e2e4f46524d71d7286689387ed7fa8872882d5
Dmitry Rodin <madiedinro@gmail.com>
2018-10-26 18:45:11 +0300

    removed skeleton

---
+++ __skeletons/README.md
@@ -0,0 +1,2 @@
+
+Look at: https://github.com/rockstat/band-skeleton-py
--- __skeletons/bandservice/bandservice/__init__.py
+++
@@ -1,7 +0,0 @@
-# Band service skeleton
-# (c) Dmitry Rodin 2018
-# ---------------------
-# this file is a reqular python module requirement
-# обязательный для python файл модуля.
-
-from . import main
